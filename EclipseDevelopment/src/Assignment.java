25d174d7cc75b3e00c038186fa2ebceca8f1d3ac
Callum <callumferguson969@gmail.com>
2021-05-29 19:44:31 +0100

    Assignment accessors

--- EclipseDevelopment/src/Assignment.java
+++ EclipseDevelopment/src/Assignment.java
@@ -12,8 +12,46 @@ public class Assignment {
 		this.score = score;
 		this.total = total;
 		this.grade = grade;
-		this.weighting = weighting;
-		
+		this.weighting = weighting;	
+	}
+
+	public String getAssignmentName() {
+		return assignmentName;
+	}
+
+	public void setAssignmentName(String assignmentName) {
+		this.assignmentName = assignmentName;
 	}
 	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public int getTotal() {
+		return total;
+	}
+	
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	
+	public double getGrade() {
+		return grade;
+	}
+	
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public int getWeighting() {
+		return weighting;
+	}
+
+	public void setWeighting(int weighting) {
+		this.weighting = weighting;
+	}
 }
