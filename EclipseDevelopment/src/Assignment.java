0cafa9c16f9cbdcdab43f0b0c4abb6e01bc42a7b
Callum <callumferguson969@gmail.com>
2021-05-29 19:07:32 +0100

    assignment constructor

--- EclipseDevelopment/src/Assignment.java
+++ EclipseDevelopment/src/Assignment.java
@@ -1,8 +1,19 @@
 
 public class Assignment {
-	int test;
-	public Assignment(int test) {
-		this.test = test;
+	
+	private String assignmentName;
+	private int score;
+	private int total;
+	private double grade;
+	private int weighting;
+	
+	public Assignment(String assignmentName, int score, int total, double grade, int weighting) {
+		this.assignmentName = assignmentName;
+		this.score = score;
+		this.total = total;
+		this.grade = grade;
+		this.weighting = weighting;
+		
 	}
 	
 }
